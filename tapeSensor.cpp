f3a381d5bcd77a5c505f765d6a6ec4e5a0bcc81f
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-09 00:07:30 +1200

    Added function documentation

--- tapeSensor.cpp
+++ tapeSensor.cpp
@@ -6,6 +6,10 @@ using namespace std;
 const int PICTURE_ROWS = 240;
 const int PICTURE_COLUMNS = 320;
 
+/**
+ * @param pixelValues Pass an vector object, the function works on this parameter by reference.
+ * @param size Pass the absolute size that the vector should be.
+ * */
 void getPixelValues(std::vector<int> &pixelValues, const int size) {
 	take_picture();
 	for (int counter = 0; counter < size; counter += 1) {
@@ -14,6 +18,13 @@ void getPixelValues(std::vector<int> &pixelValues, const int size) {
 	}
 }
 
+/**
+ * @param pixelStates Pass a vector object of type boolean; it will be worked on by reference. The function fills with
+ * true if the value in pixelValues with the corresponding index evaluates to white, otherwise the function fills with
+ * false.
+ * @param size Pass the absolute size that the pixelStates vector should be.
+ * @param pixelValues Pass a vector object of pixelValues between 0 and 255.
+ */
 void calculatePixelStates(std::vector<bool> &pixelStates, const int size, std::vector<int> pixelValues) {
 	const int MAXIMUM_BLACK_VALUE = 127;
 	for (int counter = 0; counter < pixelValues.size() - 1; counter += 1) {
@@ -27,6 +38,11 @@ void calculatePixelStates(std::vector<bool> &pixelStates, const int size, std::v
 	}
 }
 
+/**
+ * @param pixelStates Pass a vector object representing if a pixel evaluated to white (true) or black (false).
+ * @return Returns an signed integer value for the calculated error. A negative value shows that there are a higher
+ * weight of white pixels to the left, a positive value implies the same, except to the right.
+ */
 int getError(std::vector<bool> pixelStates) {
 	int totalError = 0;
 	int totalWhitePixels = 0;
@@ -40,6 +56,10 @@ int getError(std::vector<bool> pixelStates) {
 	return totalError;
 }
 
+/**
+ * @return Returns an signed integer value for the calculated error. A negative value shows that there are a higher
+ * weight of white pixels to the left, a positive value implies the same, except to the right.
+ */
 int getError(){
 	std::vector<int> pixelWhitenessValues;
 	getPixelValues(pixelWhitenessValues, PICTURE_ROWS);
