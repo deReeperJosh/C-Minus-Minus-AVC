85408cf1788d394ff25f18fcc4ee6edbb4d6fa3c
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-08 19:45:48 +1200

    Added function to calculate the error

--- tapeSensor.cpp
+++ tapeSensor.cpp
@@ -1,12 +1,12 @@
 #include "E101.h"
 #include "iostream"
 #include "vector"
-using namespace std;
 
+using namespace std;
 const int PICTURE_ROWS = 240;
 const int PICTURE_COLUMNS = 320;
 
-void getPixelValues(std::vector<int>& pixelValues, const int size) {
+void getPixelValues(std::vector<int> &pixelValues, const int size) {
 	take_picture();
 	for (int counter = 0; counter < size; counter += 1) {
 		int pixelValue = get_pixel((PICTURE_ROWS / 2), counter, 3);
@@ -14,23 +14,41 @@ void getPixelValues(std::vector<int>& pixelValues, const int size) {
 	}
 }
 
-void calculatePixelStates(std::vector<bool>& pixelStates, const int size, std::vector<int> pixelValues){
+void calculatePixelStates(std::vector<bool> &pixelStates, const int size, std::vector<int> pixelValues) {
 	const int MAXIMUM_BLACK_VALUE = 127;
-	for(int counter = 0; counter < pixelValues.size() - 1; counter += 1){
+	for (int counter = 0; counter < pixelValues.size() - 1; counter += 1) {
 		int pixelValue = pixelValues.at(counter);
-		if(pixelValue > MAXIMUM_BLACK_VALUE){
+		if (pixelValue > MAXIMUM_BLACK_VALUE) {
 			pixelValues.push_back(true);
 		}
-		else{
+		else {
 			pixelValues.push_back(false);
 		}
 	}
 }
 
-int main() {
+int getError(std::vector<bool> pixelStates) {
+	int totalError = 0;
+	int totalWhitePixels = 0;
+	for (int counter = 0; counter < pixelStates.size() - 1; counter += 1) {
+		if (pixelStates.at(counter) == true) {
+			totalWhitePixels += 1;
+			const int pixelWeight = counter - (pixelStates.sixe() / 2);
+			totalError += pixelWeight;
+		}
+	}
+	return totalError;
+}
+
+int getError(){
 	std::vector<int> pixelWhitenessValues;
-	getPixelValues(pixelWhitenessValues, PICTURE_COLUMNS);
+	getPixelValues(pixelWhitenessValues, PICTURE_ROWS);
 	std::vector<bool> pixelStateValues;
 	calculatePixelStates(pixelStateValues, PICTURE_ROWS, pixelWhitenessValues);
+	return getError(pixelStateValues);
+}
+
+int main() {
+	getError();
 	return 0;
 }
