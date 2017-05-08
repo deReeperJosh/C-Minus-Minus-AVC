176ca211787bafc2818b85144264b8658c29bdfd
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-09 01:16:37 +1200

    Added untested code for quadrant 1 of the maze

---
+++ drive.h
@@ -0,0 +1,11 @@
+#ifndef C_MINUS_MINUS_AVC_DRIVE_H
+#define C_MINUS_MINUS_AVC_DRIVE_H
+
+#include "E101.h"
+#include "tapeSensor.h"
+
+void drive();
+
+void lineDrive(const int error);
+
+#endif //C_MINUS_MINUS_AVC_DRIVE_H
