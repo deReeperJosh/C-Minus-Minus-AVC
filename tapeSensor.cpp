dc6e212966cec47e6e0373df527bf5d70d312d09
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-08 17:13:05 +1200

    Added function to take a photo and proccess into an array of whiteness values

--- tapeSensor.cpp
+++ tapeSensor.cpp
@@ -1,7 +1,21 @@
 #include "E101.h"
+using namespace std;
+
+const int PICTURE_ROWS = 240;
+const int PICTURE_COLUMNS = 320;
 
 int getError() {
-	const int picture = take_picture();
+	return get_pixel()[0];
+}
+
+int *getPixelValues() {
+	take_picture();
+	int pixelValues[PICTURE_COLUMNS];
+	for (int counter = 0; counter < PICTURE_COLUMNS; counter += 1) {
+		pixelValues[counter] = get_pixel(counter, (PICTURE_ROWS / 2), 3);
+		cout << get_pixel(counter, (PICTURE_ROWS / 2), 3);
+	}
+	return pixelValues;
 }
 
 int main() {
