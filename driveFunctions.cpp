f3bb4d3b0b011a6685397cbdd7a10925e851959b
widemouthfrog1 <widemouthfrog1@gmail.com>
2017-05-09 12:01:33 +1200

    Update driveFunctions.cpp

--- driveFunctions.cpp
+++ driveFunctions.cpp
@@ -1,24 +1,24 @@
 //driveForward is a function, given a time and speed will drive the robot forward
-void driveForward(int time, int speed){ //takes a speed between the values -254 and 254, takes a time in seconds
+void driveForward(double time, int speed){ //takes a speed between the values -254 and 254, takes a time in seconds
   set_motor(RIGHT_MOTOR,speed);//turns the right motor on
   set_motor(LEFT_MOTOR,-speed);//turns the left motor on in the opposite direction
-  sleep1(0,time*1000000); //sleep1 takes time in micro-seconds so this needs to be multiplied by 10 
+  sleep1(0,(int)(time*1000000)); //sleep1 takes time in micro-seconds so this needs to be multiplied by 10 
   stopDriving();
 
 }
-void driveBackward(int time,int speed){
+void driveBackward(double time,int speed){
   driveForward(time, -speed);
 }
-void turnRight(int time,int speed){
+void turnRight(double time,int speed){
   set_motor(RIGHT_MOTOR,STABLE_SPEED);//turns the right motor on
   set_motor(LEFT_MOTOR,-speed);//turns the left motor on in the opposite direction
-  sleep1(0,time*1000000); //sleep1 takes time in micro-seconds so this needs to be multiplied by 10 
+  sleep1(0,(int)(time*1000000)); //sleep1 takes time in micro-seconds so this needs to be multiplied by 10 
   stopDriving();
 }
-void turnLeft(int time,int speed){
+void turnLeft(double time,int speed){
   set_motor(RIGHT_MOTOR,speed);//turns the right motor on
   set_motor(LEFT_MOTOR,-STABLE_SPEED);//turns the left motor on in the opposite direction
-  sleep1(0,time*1000000); //sleep1 takes time in micro-seconds so this needs to be multiplied by 10 
+  sleep1(0,(int)(time*1000000)); //sleep1 takes time in micro-seconds so this needs to be multiplied by 10 
   stopDriving();
 }
 void stopDriving(){
