2d3b591b9d42ae1594401d4acb0e7e7af909675a
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-09 17:19:53 +1200

    Added testing function for caculatePixelStates

--- test/test_tapeSensor.cpp
+++ test/test_tapeSensor.cpp
@@ -1,15 +1,29 @@
 #include "test_tapeSensor.h"
 void test_getPixelValues(){
-	std::cout << "Creating vector of type int";
+	printf("Started test_getPixelValues\n");
+	printf("Creating vector of type int\n");
 	std::vector<int> test_pixelValues;
-	std::cout << "Calling getPixelValues";
-	getPixelValues(test_pixelValues, constants::picture::ROWS / 2);
-	std::cout << "Printing values";
-	std::cout.flush();
+	printf("Calling getPixelValues\n");
+	getPixelValues(test_pixelValues, constants::picture::ROWS);
+	printf("Printing values\n");
 	for(int test_counter = 0; test_counter < test_pixelValues.size(); test_counter += 1){
-		std::cout << "Value at: " << test_counter<< " equals: " << test_pixelValues.at(test_counter);
-		std::cout.flush();
+		printf("Value at: %i equals %i\n", test_counter, test_pixelValues.at(test_counter));
 	}
-	std::cout << "Finished test";
-	std::cout.flush();
+	printf("Finished test_getPixelValues\n");
+}
+
+void test_calculatePixelStates(){
+	printf("Started\n");
+	printf("Creating vector of type int\n");
+	std::vector<int> test_pixelValues;
+	printf("Calling getPixelValues\n");
+	getPixelValues(test_pixelValues, constants::picture::COLUMNS);
+	printf("Creating vector of type bool\n");
+	std::vector<bool> test_pixelStateValues;
+	printf("Calling getPixelStates\n");
+	calculatePixelStates(test_pixelStateValues, constants::picture::COLUMNS, test_pixelValues);
+	printf("Printing pixel states\n");
+	for(int test_counter = 0; test_counter < test_pixelValues.size(); test_counter += 1){
+		printf("States at: %i %s white\n", test_counter, test_pixelValues.at(test_counter) ? "is" : "isn't");
+	}
 }
