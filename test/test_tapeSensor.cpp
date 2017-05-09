f3e462598a26fc0b0546e62a0096a14bc8c0e326
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-09 16:06:43 +1200

    Added flush commands for printing

--- test/test_tapeSensor.cpp
+++ test/test_tapeSensor.cpp
@@ -5,11 +5,11 @@ void test_getPixelValues(){
 	std::cout << "Calling getPixelValues";
 	getPixelValues(test_pixelValues, constants::picture::ROWS / 2);
 	std::cout << "Printing values";
+	std::cout.flush();
 	for(int test_counter = 0; test_counter < test_pixelValues.size(); test_counter += 1){
-		std::cout << "Value at: ";
-		std::cout << test_counter;
-		std::cout << "Equals: ";
-		std::cout << test_pixelValues.at(test_counter);
+		std::cout << "Value at: " << test_counter<< " equals: " << test_pixelValues.at(test_counter);
+		std::cout.flush();
 	}
 	std::cout << "Finished test";
+	std::cout.flush();
 }
