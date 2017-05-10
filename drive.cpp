00c7f3e0d4b2c65088e6db14d7b892422c4806aa
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-11 11:12:54 +1200

    Removed commented code

--- drive.cpp
+++ drive.cpp
@@ -8,34 +8,8 @@ int previousError = 0
 
 void lineDrive(const int error) {
 	double scaledSpeed = (double) error * getScale(error, previousError);
-	previousError = getScale(error, previousError);
-	//int leftMotorSpeed;
-	//int rightMotorSpeed;
-	//if (scaledSpeed > 0) {
-	//	std::cout << "Scale speed > 0";
-	//	leftMotorSpeed = (int) scaledSpeed;
-	//	rightMotorSpeed = constants::vehicle::STABLE_SPEED;
-	//}
-	//else if (scaledSpeed < 0) {
-	//	std::cout << "Scale speed < 0";
-	//	leftMotorSpeed = constants::vehicle::STABLE_SPEED;
-	//	rightMotorSpeed = (int) scaledSpeed;
-	//}
-	//else {
-	//	leftMotorSpeed = constants::vehicle::STABLE_SPEED;
-	//	rightMotorSpeed = constants::vehicle::STABLE_SPEED;
-	//}
+		previousError = getScale(error, previousError);
 	turn(Math.min(254-constants::vehicle::STABLE_SPEED,(constants::vehicle::STABLE_SPEED + (int) scaledSpeed)));
-	//std::cout << "Setting left motor speed";
-	//set_motor(constants::vehicle::parts::LEFT_MOTOR, leftMotorSpeed);
-	//std::cout << "Setting right motor speed";
-	//set_motor(constants::vehicle::parts::RIGHT_MOTOR, rightMotorSpeed);
-	//std::cout << "Sleeping";
-	//sleep1(constants::vehicle::SLEEP_TIME_SECONDS, constants::vehicle::SLEEP_TIME_MICROSECONDS);
-	//std::cout << "Stopping motors";
-	//set_motor(constants::vehicle::parts::LEFT_MOTOR, 0);
-	//set_motor(constants::vehicle::parts::RIGHT_MOTOR, 0);
-	//std::cout << "Sleeping";
 }
 
 /**
