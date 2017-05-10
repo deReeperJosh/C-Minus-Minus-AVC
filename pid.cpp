f7c50f9a6e4f59734761ef5dce439b6944a99d50
d-alcala <duane.alcala1@gmail.com>
2017-05-11 10:14:06 +1200

    Almost finished pid, need derivative

--- pid.cpp
+++ pid.cpp
@@ -1,33 +1,16 @@
-//THIS PID FILE IS A PROTOTYPE
 
-//Include h files here
-
-/*
-* Problems with this file included here:
-* we need a totalError to be passed as an argument in the first place
-* need to find a integralResponse
-* need properheaders
-*
-* unsure about derivativeResponse method regarding taking previous error and current error
-* the reason is because to get a current error other than a previous error, it needs to drive straight to find
-* a difference of pixels
-*/
-int proportionalSignal(int totalError){
-  int kp = 0.5;
-  int proportionalSignal = error*kp;
-  //Debugging
-  printf("Proportional signal is: %d", proportionalSignal);
-  return proportionalSignal;
-}
-
-int derivativeResponse(int totalError){
-  int previousError = totalError;
-  int currentError = getError();
-  int kd = 5.0;
+double getScale(int error, int previousError){
+  //Proportional
+  double kp = 0.5;
+  double proportionalSignal = error*kp;
   
-  Sleep(1,0); //Sleep 1  second enough to find a significant df
-  derivativeSignal = (currentError-previousError/0.1)*kd;
-  //Debugging
-  printf("Derivative signal is: %d", derivativeSignal);
-  return derivativeSignal;
+  //Derivative
+  double kd = 5.0;
+  int previousError = 0;
+  int currentError = 0;
+  Sleep(0, 100000);
+  double derivativeSignal = (currentError - previousError/0.1)*kd;
+  
+  int finalSignal = proportionalSignal + derivativeSignal;
+  return finalSignal;
 }
