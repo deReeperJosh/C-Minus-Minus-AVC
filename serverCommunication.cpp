1fbf8104bc344b767841c3be8a03a618fbf0cc7d
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-11 01:32:00 +1200

    Added server communication files

---
+++ serverCommunication.cpp
@@ -0,0 +1,5 @@
+#include "serverCommunication.h"
+
+void openGate(){
+
+}
---
+++ serverCommunication.h
@@ -0,0 +1,8 @@
+#ifndef C_MINUS_MINUS_AVC_SERVERCOMMUNICATION_H
+#define C_MINUS_MINUS_AVC_SERVERCOMMUNICATION_H
+#include "E101.h"
+#include "config.h"
+
+void openGate();
+
+#endif //C_MINUS_MINUS_AVC_SERVERCOMMUNICATION_H
