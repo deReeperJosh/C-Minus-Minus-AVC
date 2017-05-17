a26afd3ffd3301c19a9fb02e2cd5cb18c87987ef
d-alcala <duane.alcala1@gmail.com>
2017-05-18 10:22:20 +1200

    Update wallSensor.cpp

--- wallSensor.cpp
+++ wallSensor.cpp
@@ -3,8 +3,9 @@
 
 //CHANGE THIS FUCKING METHOD, IT'S JUST THE GENERAL IDEA
 //Skeleton code
-void getErrorWalls(){
-  int threshold = 0;
+
+//void getErrorWalls(){
+  //int threshold = 0;
   //ir 1 - left
   //ir 2 - straight
   //ir 3 - right
@@ -12,13 +13,13 @@ void getErrorWalls(){
   //Get values here for IR cameras
   
   //Going above threshold means detecting walls
-  if(ir1 < threshold){
-    turnLeft();
-  }else if(ir2 < threshold){
-    straight();
-  }else if(ir3 < threshold){
-    turnRight();
-  }else{
+  //if(ir1 < threshold){
+    //turnLeft();
+  //}else if(ir2 < threshold){
+    //straight();
+  //}else if(ir3 < threshold){
+    //turnRight();
+  //}else{
     //back 
-  }
-}
+  //}
+//}
