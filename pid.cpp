7b18a971308ea5a3fd04cc9ff7322c2d0895d39b
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-17 17:08:50 +1200

    Removed broken code for pid calculation

--- pid.cpp
+++ pid.cpp
@@ -1,18 +1,17 @@
-
 /**
 * Needs to get a previous error from a last error check
 * for the derivative.
 */
-double getScale(int error, int previousError){
-  //Proportional
-  double kp = 0.5;
-  double proportionalSignal = error*kp;
-  
-  //Derivative
-  double kd = 5.0;
-  sleep1(0, 100000);
-  double derivativeSignal = (currentError - previousError/0.1)*kd;
-  
-  double finalSignal = proportionalSignal + derivativeSignal;
-  return finalSignal;
+double getScale(int error, int previousError) {
+	//Proportional
+	double kp = 0.5;
+	double proportionalSignal = error * kp;
+
+	//Derivative
+	double kd = 5.0;
+	//commented this for the moment as I'm not sure why it's here. Solomon
+	// sleep1(0, 100000);
+	double derivativeSignal = (currentError - previousError / 0.1) * kd;
+	double finalSignal = proportionalSignal + derivativeSignal;
+	return finalSignal;
 }
