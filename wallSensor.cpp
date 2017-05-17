5e3dde4dca88ba9a8ae1d2353ade5b84528049ce
d-alcala <duane.alcala1@gmail.com>
2017-05-18 10:17:35 +1200

    Update wallSensor.cpp

--- wallSensor.cpp
+++ wallSensor.cpp
@@ -1,1 +1,22 @@
 
+//Put headers here, I don't know each one of them
+
+//CHANGE THIS FUCKING METHOD, IT'S JUST THE GENERAL IDEA
+//Skeleton code
+void getErrorWalls(){
+  int threshold = 0;
+  //ir 1 - left
+  //ir 2 - straight
+  //ir 3 - right
+  
+  //Going above threshold means detecting walls
+  if(ir1 < threshold){
+    turnLeft();
+  }else if(ir2 < threshold){
+    straight();
+  }else if(ir3 < threshold){
+    turnRight();
+  }else{
+    //back 
+  }
+}
