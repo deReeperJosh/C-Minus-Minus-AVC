ceba3b7273b301da6f1e5bbea4d4695c97852776
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-17 23:50:50 +1200

    Removed depricated code

--- pid.cpp
+++ pid.cpp
@@ -1,24 +1,5 @@
 #include "pid.h"
 
-/**
-* Needs to get a previous error from a last error check
-* for the derivative.
-*/
-double getScale(int error, int previousError) {
-	//Proportional
-	double kp = 0.5;
-	double proportionalSignal = error * kp;
-
-	//Derivative
-	double kd = 5.0;
-	//commented this for the moment as I'm not sure why it's here. Solomon
-	// sleep1(0, 100000);
-	//double derivativeSignal = (currentError - previousError / 0.1) * kd;
-	//double finalSignal = proportionalSignal + derivativeSignal;
-	//return finalSignal;
-	return 0;
-}
-
 double calculateProportionalSignal(const int error) {
 	double proportionalSignal = error * constants::PID::PROPORTIONAL_CONSTANT;
 	return proportionalSignal;
@@ -29,7 +10,7 @@ double calculateDerivativeSignal(const int error, const int previousError) {
 	return derivativeSignal;
 }
 
-double calculateIntegralSignal(const int totalErrorExperienced){
+double calculateIntegralSignal(const int totalErrorExperienced) {
 	double integralSignal = totalErrorExperienced * constants::PID::INTEGRAL_CONSTANT;
 	return integralSignal;
 }
