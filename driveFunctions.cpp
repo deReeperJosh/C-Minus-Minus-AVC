109d66dbc21a0aec43b8b52338f38453a4955dbc
widemouthfrog1 <widemouthfrog1@gmail.com>
2017-05-18 10:18:28 +1200

    Update driveFunctions.cpp

--- driveFunctions.cpp
+++ driveFunctions.cpp
@@ -3,7 +3,7 @@
 #include "driveFunctions.h"
 
 void driveForward(int speed) { //takes a speed between the values -254 and 254
-	set_motor(constants::vehicle::parts::RIGHT_MOTOR, speed);
+	set_motor(constants::vehicle::parts::RIGHT_MOTOR, -speed);
 	set_motor(constants::vehicle::parts::LEFT_MOTOR, -speed);
 	driveSleep();
 	stopDriving();
@@ -14,15 +14,15 @@ void driveBackward(int speed) {
 }
 
 void turnRight(int speed) {
-	set_motor(constants::vehicle::parts::RIGHT_MOTOR, constants::vehicle::STABLE_SPEED);
-	set_motor(constants::vehicle::parts::LEFT_MOTOR, -speed);
+	set_motor(constants::vehicle::parts::RIGHT_MOTOR, -constants::vehicle::STABLE_SPEED);
+	set_motor(constants::vehicle::parts::LEFT_MOTOR, speed);
 	driveSleep();
 	stopDriving();
 }
 
 void turnLeft(int speed) {
-	set_motor(constants::vehicle::parts::RIGHT_MOTOR, speed);
-	set_motor(constants::vehicle::parts::LEFT_MOTOR, -constants::vehicle::STABLE_SPEED);
+	set_motor(constants::vehicle::parts::RIGHT_MOTOR, -speed);
+	set_motor(constants::vehicle::parts::LEFT_MOTOR, constants::vehicle::STABLE_SPEED);
 	driveSleep();
 	stopDriving();
 }
