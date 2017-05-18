36696e8479be8a1b79947af15a78addba5c5f8f3
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2017-05-18 12:51:14 +1200

    Added debugging code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
 	init();
 	take_picture();
-	printf("Pixels: %i\n%i\n%i\n%i\n%i", get_pixel(123, 123, 3), get_pixel(23, 23, 3), get_pixel(56, 56, 3), get_pixel(48, 48, 3), get_pixel(99, 99, 3));
-	//drive();
+	printf("Pixels: %i\n%i\n%i\n%i\n%i\nEnd test\n", get_pixel(123, 123, 3), get_pixel(23, 23, 3), get_pixel(56, 56, 3), get_pixel(48, 48, 3), get_pixel(99, 99, 3));
+	drive();
 	return 0;
 }
