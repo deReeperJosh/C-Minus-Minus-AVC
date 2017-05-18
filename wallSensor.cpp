9edb65d4bdc6597805dae3a9ecf90503b2156a41
d-alcala <duane.alcala1@gmail.com>
2017-05-18 17:08:02 +1200

    Update wallSensor.cpp

--- wallSensor.cpp
+++ wallSensor.cpp
@@ -1,32 +1,24 @@
 
 //Put headers here, I don't know each one of them
 
-//CHANGE THIS FUCKING METHOD, IT'S JUST THE GENERAL IDEA
+//CHANGE THIS METHOD, IT'S JUST THE GENERAL IDEA
 //Skeleton code
 
 void getErrorWallks(){
   int irReading1;
   int irReading2;
   int irReading3;
-    
+  int threshold = 100;
+  
+  irReading1 = read_analog();
+  irReading2 = read_analog();
+  irReading3 = read_analog();
+  
+  if(irReading2 > threshold){
+    //forward
+  }else if(irReading1 > threshold){
+    //left
+  }else if(irReading3 > threshold){
+     //right
+  } 
 }
-
-//void getErrorWalls(){
-  //int threshold = 0;
-  //ir 1 - left
-  //ir 2 - straight
-  //ir 3 - right
-  
-  //Get values here for IR cameras
-  
-  //Going above threshold means detecting walls
-  //if(ir1 < threshold){
-    //turnLeft();
-  //}else if(ir2 < threshold){
-    //straight();
-  //}else if(ir3 < threshold){
-    //turnRight();
-  //}else{
-    //back 
-  //}
-//}
